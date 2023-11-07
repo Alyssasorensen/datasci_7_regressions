f89811d785406f6c9a6b909e11820b45f635ed48
Alyssasorensen <-m>
2023-11-07 21:40:22 +0000

    updated .py file

--- regression_multiple.py
+++ regression_multiple.py
@@ -1,65 +1,123 @@
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
-
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from statsmodels.stats.diagnostic import linear_rainbow
-from scipy.stats import shapiro
-from statsmodels.stats.diagnostic import het_goldfeldquandt
 
+url = "https://raw.githubusercontent.com/Alyssasorensen/datasci_7_regressions/main/datasets/Cancer_Rates%20(1).csv"
+df = pd.read_csv(url)
+df
 
-# Sample data
-data = {
-    'BMI': [24.679912, 34.186786, 30.577900, 28.377865, 21.074308, 26.647627, 27.125092, 25.554427, 18.919416, 20.280209],
-    'Age': [25, 34, 30, 28, 21, 26, 27, 25, 18, 20],
-    'Blood Pressure': [117.455103, 129.785142, 126.325654, 112.628953, 110.513102, 115.892390, 120.302129, 120.037400, 109.762577, 114.556229]
-}
-df = pd.DataFrame(data)
+## Dependent Variable (Y):
 
-# Fit the regression model
-X = df[['BMI', 'Age']]
-X = sm.add_constant(X)  # Adds a constant term to the predictor
-y = df['Blood Pressure']
-model = sm.OLS(y, X)
-results = model.fit()
+## Dependent Variable: All_Cancer
 
-# Print summary of the regression
-print(results.summary())
+## Independent Variables (X):
 
+# Independent Variable 1: Colorectal
+# Independent Variable 2: Lung_Bronc
+# Independent Variable 3: Breast_Can
+# Independent Variable 4: Prostate_C
+# Independent Variable 5: Urinary_Sy
 
-residuals = results.resid
-fitted = results.fittedvalues
+# Define the dependent variable (Y) and independent variables (X)
+Y = df['All_Cancer']
+X = df[['Colorectal', 'Lung_Bronc', 'Breast_Can', 'Prostate_C', 'Urinary_Sy']]
 
+# Add a constant term (intercept) to the model
+X = sm.add_constant(X)
 
-##### CHECKING ASSUMPTIONS #####
+# Fit the multiple linear regression model
+model = sm.OLS(Y, X).fit()
 
-# Checking multicollinearity using VIF
+# Get the model summary
+print(model.summary())
+
+# Calculate VIF for each independent variable
 vif_data = pd.DataFrame()
 vif_data['Variable'] = X.columns
 vif_data['VIF'] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+
+# Print the VIF data
 print("\nVIF Data:")
 print(vif_data)
 
-### Assessing linearity of the relationship
-stat, p_value = linear_rainbow(results)
-print(f"Rainbow Test: stat={stat}, p-value={p_value}")
+## Results Interpreted:
 
-## A significant p-value indicates that the relationship is not linear.
-# Plot observed vs fitted values
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=df['Blood Pressure'], y=fitted)
-plt.xlabel('Observed Values')
-plt.ylabel('Fitted Values')
-plt.title('Observed vs Fitted Values')
-plt.plot([min(df['Blood Pressure']), max(df['Blood Pressure'])], [min(fitted), max(fitted)], color='red', linestyle='--')
+# Constant (Intercept) VIF = 17.071047
+
+# The constant term, or intercept, typically has a high VIF because it is not related to any other variable in the model. You can ignore its VIF value.
+
+# Colorectal, VIF = 3.098965:
+
+# The VIF of Colorectal is relatively low (below 5), suggesting that it has little multicollinearity with the other variables in the model.
+
+# Lung_Bronc, VIF = 5.131104:
+
+# The VIF of Lung_Bronc is moderate (around 5), indicating some degree of multicollinearity with other variables but not extremely high.
+
+# Breast_Can, VIF = 2.754088:
+
+# The VIF of Breast_Can is low (below 5), indicating little multicollinearity with other variables.
+
+# Prostate_C, VIF = 3.697852:
+
+# The VIF of Prostate_C is relatively low (below 5), suggesting limited multicollinearity.
+
+# Urinary_Sy, VIF = 4.272061:
+
+# The VIF of Urinary_Sy is moderate (around 4), indicating some multicollinearity but not extremely high.
+
+# Define the independent variables (e.g., X1, X2, X3)
+X1 = df['Colorectal']
+X2 = df['Lung_Bronc']
+X3 = df['Breast_Can']
+
+# Define the dependent variable (Y)
+Y = df['All_Cancer']
+
+# Create scatterplots
+plt.figure(figsize=(12, 6))
+
+plt.subplot(131)
+plt.scatter(X1, Y)
+plt.xlabel('Colorectal')
+plt.ylabel('All_Cancer')
+
+plt.subplot(132)
+plt.scatter(X2, Y)
+plt.xlabel('Lung_Bronc')
+plt.ylabel('All_Cancer')
+
+plt.subplot(133)
+plt.scatter(X3, Y)
+plt.xlabel('Breast_Can')
+plt.ylabel('All_Cancer')
+
+plt.tight_layout()
 plt.show()
 
+# Fit the multiple linear regression model
+model = sm.OLS(Y, X).fit()
 
+# Get the fitted values (predicted values) from the model
+fitted_values = model.fittedvalues
 
+# Create a scatterplot of observed vs. fitted values
+plt.figure(figsize=(10, 6))
+plt.scatter(Y, fitted_values, c='blue', label='Observed vs. Fitted Values')
+plt.plot([Y.min(), Y.max()], [Y.min(), Y.max()], 'k--', lw=2, color='red', label='Ideal Line')
+plt.xlabel('Observed Values (Y)')
+plt.ylabel('Fitted Values')
+plt.title('Observed vs. Fitted Values')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-### Assessing normality of the residuals
+## Assessing normality of the residuals
 W, p_value = shapiro(residuals)
 print(f"Shapiro-Wilk Test: W={W}, p-value={p_value}")
 
@@ -69,24 +127,38 @@ stats.probplot(residuals, plot=plt)
 plt.title('Q-Q Plot of Residuals')
 plt.show()
 
+## Assessing the homogeneity of variance of the residuals
 
+# Fit the multiple linear regression model
+model = sm.OLS(Y, X).fit()
 
+# Get the residuals from the model
+residuals = model.resid
 
-##### Assessing the homogeneity of variance of the residuals
-gq_test = het_goldfeldquandt(residuals, results.model.exog)
-print(f"Goldfeld-Quandt Test: F-statistic={gq_test[0]}, p-value={gq_test[1]}")
-
-# a significant p-value indicates heteroscedasticity, meaning that the variance of the 
-# residuals is not constant across different levels of the independent variable(s).
+# Create a Residuals vs. Fitted Values plot
+plt.figure(figsize=(10, 6))
+sns.residplot(x=model.fittedvalues, y=residuals, lowess=True, scatter_kws={'alpha': 0.5})
+plt.xlabel('Fitted Values')
+plt.ylabel('Residuals')
+plt.title('Residuals vs. Fitted Values')
+plt.grid(True)
+plt.show()
 
 # Plot residuals vs fitted values
+
+# Fit the multiple linear regression model
+model = sm.OLS(Y, X).fit()
+
+# Get the residuals from the model
+residuals = model.resid
+
+# Create a Residuals vs. Fitted Values plot
 plt.figure(figsize=(10, 6))
-sns.scatterplot(x=fitted, y=residuals)
+sns.scatterplot(x=model.fittedvalues, y=residuals, color='blue', alpha=0.5)
 plt.axhline(0, color='red', linestyle='--')
 plt.xlabel('Fitted Values')
 plt.ylabel('Residuals')
-plt.title('Residuals vs Fitted Values')
+plt.title('Residuals vs. Fitted Values')
+plt.grid(True)
 plt.show()
-
-
 
